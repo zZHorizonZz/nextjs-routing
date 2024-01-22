16bce05aa43fb7b020cd79aa56a1f662e266699d
zZHorizonZz <danfiala23@gmail.com>
2024-01-22 09:09:52 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/test', // If this is set then and we got to localhost/test then we get the 404 page, If this is not set then we get the index page
+};
 
 export default nextConfig;
